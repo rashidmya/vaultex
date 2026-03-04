59bf18fd8b235116aba4e0acab59a27a60ddcbb9
rashidmya <bugadev@proton.me>
2026-03-05 02:09:45 +0400

    unused removed

--- source/js/sidebar-left.js
+++ source/js/sidebar-left.js
@@ -48,7 +48,6 @@ on(toggleLeftBtn, 'click', function () {
   if (!sidebarLeft) return;
   if (sidebarLeft.classList.contains('collapsed')) { openLeft(); } else { closeLeft(); }
 });
-on($('#sidebar-left-close'), 'click', closeLeft);
 
 /* Restore sidebar state (desktop only) */
 if (!isMobile()) {
